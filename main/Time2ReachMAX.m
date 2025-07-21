8309de0b2ff42fe0cbc8ab5078c2ac9cf8c7ddce
Yeidi Bautista <bautista.yeet@gmail.com>
2025-07-21 15:26:24 -0700

    Calculate time to reach a full state of charge.

---
+++ main/Time2ReachMAX.m
@@ -0,0 +1,20 @@
+%% - Compute Time to reach Full SOC - 
+% This function calculates the time it takes for the capacitor to reach 
+% 99.9% state of charge (SOC).
+
+% Note: The RC equation V(t) = Vmax * (1 - e^(-t/tau)), when V(t) = Vmax,
+% results in t = inf. Thus, we instead use V(t) = 0.999  * V_max
+
+%% Inputs
+% V_max: the charging voltage (or maximmum voltage/100% SOC) (Volts)
+% tau: time constant (seconds)
+
+%% Outputs
+% t_MAX: time it takes to reach 99.9% SOC
+
+%% Equations
+% t_MAX = -tau * ln(1 - V_target/V_max);
+function t_MAX = Time2ReachMAX(V_max,tau)
+    V_full = 0.999 * V_max;
+    t_MAX = -tau * log(1 - V_full / V_max);
+end
